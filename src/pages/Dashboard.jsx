7abd0de16e14ab0209aea4f95e06ffd0d334ceb7
Dolapo Adebanjo <dolapomola@gmail.com>
2023-11-18 10:59:14 +0100

    building and styling of second page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Sidebar from '../components/Dashboard/Sidebar';
+import Home from './Home';
+import '../compiled/dashboard.css';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className='dashboard_container'>
+        <div className='flex_one'>
+          <Sidebar />
+        </div>
+        <div className='flex_two'>
+          <Home />
+        </div>
+   
+</div>
+)
+};
 
 export default Dashboard
